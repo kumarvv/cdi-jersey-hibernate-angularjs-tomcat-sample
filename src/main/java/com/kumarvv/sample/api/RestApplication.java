4e057f1f3323af828df774194ba7c1aa573a92e2
vij2ee <vij2ee@gmail.com>
2014-10-01 22:32:57 -0400

    initial version

---
+++ src/main/java/com/kumarvv/sample/api/RestApplication.java
@@ -0,0 +1,28 @@
+package com.kumarvv.sample.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * REST Application
+ */
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+	private Set<Object> singletons = new HashSet<>();
+	private Set<Class<?>> classes = new HashSet<>();
+
+	public RestApplication(){
+		classes.add(AirportAPI.class);
+	}
+	@Override
+	public Set<Class<?>> getClasses() {
+		return classes;
+	}
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+}
